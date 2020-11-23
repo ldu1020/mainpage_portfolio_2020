cc74e71cd5d8995e6af9ff4be000f6c6bfe55a3b
ldu1020 <66596880+ldu1020@users.noreply.github.com>
2020-11-23 17:54:46 +0900

    add ajax animation and navgation

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
-const testdiv = document.querySelector('.ani_test')
-testdiv.addEventListener('click',()=>{
-  const a = document.createElement('div');
-  a.setAttribute('class',"item")
-  testdiv.appendChild(a)
-  a.style.width ="30%"
-  a.style.height ="30%"
-  a.style.backgroundColor="white"
+/** @format */
 
-})
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('.navigation');
+const navList = document.querySelectorAll('.navigation__lists li');
+const beforeOnLoadAni = document.querySelector('.beforeOnLoadAnimation');
+const onLoadAni = document.querySelector('.onLoadAniMation');
+const article = document.querySelector('.article');
 
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('change');
+  nav.classList.toggle('visible');
+});
+
+beforeOnLoadAni.addEventListener('animationend', () => {
+  beforeOnLoadAni.style.display = 'none';
+  onLoadAni.style.display = 'inline';
+  hamburgerMenu.classList.toggle('change');
+  nav.classList.toggle('visible');
+});
+
+onLoadAni.addEventListener('animationend', () => {
+  onLoadAni.style.display = 'none';
+});
+
+navList.forEach((li) => {
+  li.addEventListener('click', (event) => {
+    event.preventDefault();
+    beforeOnLoadAni.style.display = 'inline';
+    switch (li.textContent) {
+      case 'HOME':
+        FetchData('./home');
+        return;
+      case 'PROJECT':
+        FetchData('./project');
+        return;
+    }
+  });
+});
+
+function FetchData(path) {
+  fetch(path).then((data) => {
+    data.text().then((text) => {
+      article.innerHTML = text;
+    });
+  });
+  history.pushState('', '', path);
+}
