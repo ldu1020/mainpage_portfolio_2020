f51428d1ae582b675d26d8ce8cc343a8e5380367
ldu1020 <66596880+ldu1020@users.noreply.github.com>
2020-11-27 00:42:49 +0900

    add mediaquery and project slide and navigation logic

--- main.js
+++ main.js
@@ -5,44 +5,149 @@ const nav = document.querySelector('.navigation');
 const navList = document.querySelectorAll('.navigation__lists li');
 const beforeOnLoadAni = document.querySelector('.beforeOnLoadAnimation');
 const onLoadAni = document.querySelector('.onLoadAniMation');
-const article = document.querySelector('.article');
+const content = document.querySelector('.content');
+const routes = {
+  '/': './home.html',
+  '/project': '/project.html',
+};
+
+FetchData('/');
+
+const mobileBoolean = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+  navigator.userAgent
+)
+  ? true
+  : false;
+
+window.addEventListener('popstate', (event) => {
+  console.log('[popstate]', event.state);
+  FetchData(event.state.path);
+});
 
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('change');
   nav.classList.toggle('visible');
 });
 
+let path;
+
 beforeOnLoadAni.addEventListener('animationend', () => {
+  console.log('event basic');
   beforeOnLoadAni.style.display = 'none';
   onLoadAni.style.display = 'inline';
-  hamburgerMenu.classList.toggle('change');
-  nav.classList.toggle('visible');
 });
 
 onLoadAni.addEventListener('animationend', () => {
   onLoadAni.style.display = 'none';
+  beforeOnLoadAni.removeEventListener('animationend', unloadEvent);
+  console.log('remove path');
 });
+
+function unloadEvent() {
+  console.log('event fetch');
+  window.innerWidth < 750 && hamburgerMenu.classList.toggle('change');
+  window.innerWidth < 750 && nav.classList.toggle('visible');
+  FetchData(path);
+}
 
 navList.forEach((li) => {
   li.addEventListener('click', (event) => {
     event.preventDefault();
+    path = event.target.getAttribute('href');
+    beforeOnLoadAni.addEventListener('animationend', unloadEvent);
     beforeOnLoadAni.style.display = 'inline';
-    switch (li.textContent) {
-      case 'HOME':
-        FetchData('./home');
-        return;
-      case 'PROJECT':
-        FetchData('./project');
-        return;
-    }
+    history.pushState({ path }, null, path);
   });
 });
 
-function FetchData(path) {
-  fetch(path).then((data) => {
-    data.text().then((text) => {
-      article.innerHTML = text;
+async function FetchData(path) {
+  try {
+    const url = routes[path];
+    const res = await fetch(url);
+    content.innerHTML = await res.text();
+
+    switch (path) {
+      case '/project':
+        initProject();
+        nav.style.backgroundColor = '#4b4b4b';
+        document.querySelectorAll('.navigation__lists li').forEach((li) => {
+          li.style.color = '#fff';
+        });
+        return;
+
+      default:
+        nav.style.backgroundColor = '#fff';
+        document.querySelectorAll('.navigation__lists li').forEach((li) => {
+          li.style.color = '#4b4b4b';
+        });
+        return;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function initProject() {
+  !mobileBoolean &&
+    VanillaTilt.init(document.querySelectorAll('.project__card'));
+  addProjectDetail();
+  addProjectSlide();
+}
+
+function addProjectDetail() {
+  const BtnsCardDetail = document.querySelectorAll(
+    '.project__card-button--detail'
+  );
+
+  BtnsCardDetail.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      const targetChildren = event.target.parentNode.parentNode.children;
+      const infoMain = targetChildren[0];
+      const infoDetail = targetChildren[1];
+      infoMain.classList.toggle('invisible');
+      infoDetail.classList.toggle('invisible');
+      console.log(btn.textContent);
+      btn.textContent =
+        btn.textContent === '상세설명' ? '돌아가기' : '상세설명';
     });
   });
-  history.pushState('', '', path);
+}
+
+function addProjectSlide() {
+  const projectCards = document.querySelectorAll('.project__card');
+  projectCards.forEach((card) => {
+    const imgWrapper = card.children[1];
+    const info = card.children[2];
+    const controller = card.children[3];
+
+    // Slide
+    const imgWrapperWidth = imgWrapper.getBoundingClientRect().width;
+    const imgLists = imgWrapper.children[0];
+    const imgListLength = imgLists.children.length;
+
+    imgLists.style.width = `${imgWrapperWidth * imgListLength}px`;
+    imgLists.style.display = 'flex';
+
+    const buttons = controller.children;
+    const pre = buttons[0];
+    const next = buttons[1];
+    let flag = 0;
+
+    pre.addEventListener('click', () => {
+      if (flag !== 0) {
+        flag += 1;
+      }
+      const move = (100 / imgListLength) * flag;
+      imgLists.style.transform = `translate(${move}%)`;
+    });
+
+    next.addEventListener('click', () => {
+      if (flag + imgListLength > 1) {
+        flag -= 1;
+      }
+      const move = (100 / imgListLength) * flag;
+      imgLists.style.transform = `translate(${move}%)`;
+      console.log();
+    });
+  });
 }
